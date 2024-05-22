122179683a80afadf18a41a37ab14c39a1479903
MubarakshinRuslan <mubarakshin.r.r@yandex.ru>
2024-05-22 11:33:55 +0500

    Лабораторная работа №2 (день 2)

---
+++ Lab2/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Factorial
+    {
+        public static long Ffactorial(int n)
+        {
+            long res = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
+
+        public static long Rfactorial(int n) => n < 1 ? 1 : n * Rfactorial(n - 1);
+    }
+}
--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -6,10 +6,15 @@ namespace Module3
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите число ворон на ветке: ");
-            int n = int.Parse(Console.ReadLine());
-            var crows = new Crows(n);
-            Console.WriteLine(crows.GetCount());
+            //Console.Write("Введите число ворон на ветке: ");
+            //int n = int.Parse(Console.ReadLine());
+            //var crows = new Crows(n);
+            //Console.WriteLine(crows.GetCount());
+
+            Console.WriteLine("Введите число, факториал которого Вы хотите вычислить: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Вычисление по Ffactorial: {number}! = {Factorial.Ffactorial(number)}");
+            Console.WriteLine($"Вычисление по Rfactorial: {number}! = {Factorial.Rfactorial(number)}");
         }
     }
 }
