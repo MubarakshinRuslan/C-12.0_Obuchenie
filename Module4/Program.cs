0d39505df584e99c82d12da0be3de043e9435673
Mubarakshin Ruslan <mubarakshin.r.r@yandex.ru>
2024-05-24 20:55:05 +0500

    Модуль 4 лабораторная (день 2)

---
+++ Module4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Module4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            foreach (var arg in args)
+            {
+                Console.Write(arg == args.Last() ? $"{arg} = " : $"{arg} + ");
+                sum += int.Parse(arg);
+            }
+            Console.Write(sum);
+        }
+    }
+}
