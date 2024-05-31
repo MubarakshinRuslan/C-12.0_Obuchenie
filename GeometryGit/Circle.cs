d05f1ab1a7060b892aca0aa644a4e5241c075232
Mubarakshin Ruslan <mubarakshin.r.r@yandex.ru>
2024-05-31 21:49:06 +0500

    Перегрузка операторов класса Circle (день 5)

---
+++ GeometryGit/Circle.cs
@@ -0,0 +1,86 @@
+﻿namespace Geometry
+{
+    class Circle
+    {
+        Fdata fd;
+        private double x;
+        public double X
+        {
+            get => x;
+            set => x = value;
+        }
+
+        private double y;
+        public double Y
+        {
+            get => y;
+            set => y = value;
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                if (value > 0)
+                    radius = value;
+                else
+                    throw new ArgumentException("radius <= 0");
+            }
+        }
+
+        public double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public Circle(int x, int y, int radius)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+
+        public void PrintInfo()
+        {
+            Console.Write("Circle:" + X + "," + Y + " radius=" + Radius);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Circle c)
+                return this.Area() == c.Area();
+            else
+                return base.Equals(obj);
+        }
+
+        public int CompareTo(Circle c)
+        {
+            if (this.Area() > c.Area()) return 1;
+            if (this.Area() < c.Area()) return -1;
+            return 0;
+        }
+
+        public override int GetHashCode()
+        {
+            return (X, Y, Radius).GetHashCode();
+        }
+
+        public static bool operator == (Circle c1, Circle c2)
+        {
+            return object.Equals(c1, c2);
+        }
+
+        public static bool operator != (Circle c1, Circle c2)
+        {
+            return !(c1 == c2);
+        }
+    }
+}
---
+++ GeometryGit/Romb.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    class Romb
+    {
+        Fdata fd;
+        public double a
+        {
+            get => fd.a;
+            set => fd.a = value;
+        }
+
+        public double b
+        {
+            get => fd.b;
+            set => fd.b = value;
+        }
+
+        public double Area()
+        {
+            return (a * 2 + b * 2) / 2.0; 
+        }
+
+        public Romb(Fdata fd)
+        {
+            this.fd = fd;
+            fd.type = Figures.Romb;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            fd.x0 += dx;
+            fd.y0 += dy;
+        }
+
+        public void PrintInfo()
+        {
+            Console.Write(fd.type + ":" + fd.x0 + "," + fd.y0 + " color=" + fd.color);
+            Console.WriteLine(" a=" + a + " b=" + b);
+        }
+    }
+}
